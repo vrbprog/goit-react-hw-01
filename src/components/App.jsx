594294dcbb8a8eea8b29f0d632bbe6c775109c69
Roman <vrbox@i.ua>
2024-12-09 12:05:14 +0200

    Upgrate structure

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+
+import Product from './Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,8 @@
+export default function Product() {
+  return (
+    <div>
+      <h2>Cookies</h2>
+	  <p>Price: 999 credits</p>
+    </div>
+  );
+}; 
